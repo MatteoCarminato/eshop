1305b49b84f7be08dc27e584e2539339a76e386a
Matteo Carminato <mcarminato95@gmail.com>
2026-06-20 13:54:48 -0300

    wip

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -514,6 +514,7 @@ class WalletController extends Controller
         $spreadsheet = $reader->load($templatePath);
         $sheet       = $spreadsheet->getActiveSheet();
 
+            dd('a');
         // ---- Linha 1: saldos
         $sheet->setCellValue('F1', 'Saldo ' . $client->name . ' R$ ' . $br($brl));
         $sheet->setCellValue('I1', 'U$ ' . $br($usd));
