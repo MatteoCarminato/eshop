e1eb42303f930d5c7c95009094ce808d16e29ec2
Matteo Carminato <mcarminato95@gmail.com>
2026-07-11 17:51:29 -0300

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
                         <div class="text-center mt-sm-5 mb-4 text-white-50">
                             <div>
                                 <a href="index.html" class="d-inline-block auth-logo">
-                                    <img src="assets/images/logo-light.png" alt="" height="20">
+                                    <img src="assets/images/logo-eshop.png" alt="" height="60">
                                 </a>
                             </div>
                         </div>
