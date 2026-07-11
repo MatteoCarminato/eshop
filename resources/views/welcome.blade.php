1431f5572a35dc68e773d4d286e7b0ead1cdf38d
Matteo Carminato <mcarminato95@gmail.com>
2026-07-11 17:56:31 -0300

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,63 +1,47 @@
 <!doctype html>
-<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg"
-    data-sidebar-image="none" data-preloader="disable" data-theme="default" data-theme-colors="default">
+<html lang="pt-BR" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg"
+    data-sidebar-image="none" data-preloader="disable" data-theme="default" data-theme-colors="default"
+    data-bs-theme="dark">
 
 <head>
-
     <meta charset="utf-8" />
     <title>Eshop Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
-
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
-
-    <script src="assets/js/layout.js"></script>
-
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-
-    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
-
-    <link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" />
-
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/auth-eshop.css') }}" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
-
+<body class="auth-eshop">
     <div class="auth-page-wrapper pt-5">
-        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-            <div class="bg-overlay"></div>
-
-            <div class="shape">
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 0 1440 120">
-                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
-                </svg>
-            </div>
-        </div>
-
-        <!-- auth page content -->
+        <div id="auth-particles" class="auth-particles-bg"></div>
         <div class="auth-page-content">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text-center mt-sm-5 mb-4 text-white-50">
                             <div>
-                                <a href="index.html" class="d-inline-block auth-logo">
-                                    <img src="assets/images/logo-eshop.png" alt="" height="60">
+                                <a href="{{ url('/') }}" class="d-inline-block auth-logo">
+                                    <img src="{{ asset('assets/images/logo-eshop.png') }}" alt="eShop" height="70">
                                 </a>
                             </div>
+                            <p class="mt-3 fs-15 fw-medium">Acesso ao sistema</p>
                         </div>
                     </div>
                 </div>
-
                 <div class="row justify-content-center">
                     <div class="col-md-8 col-lg-6 col-xl-5">
-                        <div class="card mt-4 card-bg-fill">
-
+                        <div class="card mt-4">
                             <div class="card-body p-4">
+                                <div class="text-center mt-2">
+                                    <h5>Bem-vindo!</h5>
+                                    <p class="text-muted">Faça login para acessar o sistema.</p>
+                                </div>
                                 <div class="p-2 mt-4">
                                     @if(session('error'))
                                         <div class="alert alert-danger">{{ session('error') }}</div>
@@ -81,11 +65,28 @@
                                         </div>
                                         <div class="mb-3">
                                             <label class="form-label" for="password">Senha</label>
-                                            <input type="password" class="form-control" id="password" name="password"
-                                                placeholder="Digite sua senha" required>
+                                            <div class="position-relative auth-pass-inputgroup mb-3">
+                                                <input type="password" class="form-control password-input" id="password"
+                                                    name="password" placeholder="Digite sua senha" required>
+                                                <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none password-addon"
+                                                    type="button" id="password-addon">
+                                                    <i class="ri-eye-fill align-middle"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="form-check d-flex align-items-center justify-content-between mb-3">
+                                            <div>
+                                                <input class="form-check-input" type="checkbox" value="1" id="remember"
+                                                    name="remember">
+                                                <label class="form-check-label" for="remember">Lembrar-me</label>
+                                            </div>
+                                            @if (Route::has('password.request'))
+                                                <a href="{{ route('password.request') }}"
+                                                    class="link-eshop fs-13">Esqueceu a senha?</a>
+                                            @endif
                                         </div>
                                         <div class="mt-4">
-                                            <button class="btn btn-success w-100" type="submit">Entrar</button>
+                                            <button class="btn btn-eshop w-100" type="submit">Entrar</button>
                                         </div>
                                     </form>
                                 </div>
@@ -109,10 +110,11 @@
             </div>
         </footer>
     </div>
-
-    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/libs/feather-icons/feather.min.js"></script>
-    <script src="assets/js/pages/password-addon.init.js"></script>
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/particles.js/particles.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/particles.app.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/password-addon.init.js') }}"></script>
 </body>
 
 </html>
